4844016eefb0bc1c7e181113daddb10bf4929238
Hiroyuki-Ishige <hiroyukiishige@hotmail.com>
2023-05-05 06:43:19 +0700

    amend

--- main.py
+++ main.py
@@ -19,36 +19,54 @@ https://tat-pytone.hatenablog.com/entry/2021/09/26/131945 ### add watermark
 import tkinter
 from tkinter import *
 from tkinter import filedialog
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw, ImageFont
 
 ### fixed variable
 WIDTH = 640
 HEIGHT = 400
 
 
-### Function to upload
+# Function to upload
 def upload_clicked():
     global img1_tk  # need to set as global variable because main window is outside of this function
 
-    ### import image file
+    # import image file
     filename = filedialog.askopenfilename(initialdir="C:/Users/Hiroyuki Ishige/Pictures", title="Selected File",
                                           filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
-    ### set size of image
-    img1 = Image.open(filename)
+    # set size of image and convert to RGBA
+    img1 = Image.open(filename).convert("RGBA")
     pic_width = int(img1.width / 6)
     pic_height = int(img1.height / 6)
 
-    ### resize image
-    img1_tk = ImageTk.PhotoImage(image=img1.resize((pic_width, pic_height)))
+    # set empty image to write watermark word. Same size of img1.
+    txt = Image.new("RGBA", img1.size, (255, 255, 255, 0))
 
-    ### Create Canvas_1
+    # set font
+    fnt1 = ImageFont.truetype("meiryo", int(img1.size[0] / 10))
+
+    # Set image context
+    d = ImageDraw.Draw(txt)
+
+    # draw watermark with transparency
+    color = (255, 255, 255, 128)  # 50% transparency
+    d.text((int(img1.size[0] * 0.1), (int(img1.size[1] * 0.2))), input_watermark.get(),
+           font=fnt1,
+           fill=color)
+
+    # alpha brend of image and watermark
+    out = Image.alpha_composite(img1, txt)
+
+    # resize image
+    img1_tk = ImageTk.PhotoImage(image=out.resize((pic_width, pic_height)))
+
+    # Create Canvas_1
     canvas_1 = Canvas(window, width=pic_width, height=pic_height, bg="white")
     canvas_1.grid(column=0, row=2)
 
-    ### show image on cavas
+    # show image on cavas
     canvas_1.create_image(0, 0, anchor="nw", image=img1_tk)
 
-    ### print watermark text
+    # print watermark text
     print(input_watermark.get())
 
 
@@ -75,7 +93,6 @@ label_watermark.config(padx=20, pady=20)
 input_watermark = Entry(width=20)
 input_watermark.grid(column=1, row=1)
 input_watermark.insert(tkinter.END, "Please fill in")
-
 
 ### event roop
 window.mainloop()
